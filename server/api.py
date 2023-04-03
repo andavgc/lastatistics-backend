7eb3f1c2eb88a27404ba2cefa1ed9da2f819aba4
Alfredo <aacanizales.13@est.ucab.edu.ve>
2023-04-02 23:51:55 -0300

    borrando repetido

--- server/api.py
+++ server/api.py
@@ -20,15 +20,6 @@ origins = [
     "http://localhost:8080",
     "http://0.0.0.0:8000/"
 ]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
